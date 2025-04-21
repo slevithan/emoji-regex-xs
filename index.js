95cf368a20b809086f22f4ab74e680ac764dce7c
Steven Levithan <steves_list@hotmail.com>
2025-04-21 16:42:52 +0200

    Use slightly expanded pattern from official Unicode docs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const r = String.raw;
-const seq = r`(?:\p{Emoji}\uFE0F\u20E3?|\p{Emoji_Modifier_Base}\p{Emoji_Modifier}?|\p{Emoji_Presentation})`;
-const sTags = r`\u{E0061}-\u{E007A}`;
-module.exports = () => new RegExp(r`[\u{1F1E6}-\u{1F1FF}]{2}|\u{1F3F4}[${sTags}]{2}[\u{E0030}-\u{E0039}${sTags}]{1,3}\u{E007F}|${seq}(?:\u200D${seq})*`, 'gu');
+const r = String.raw,
+  seq = r`\p{RI}{2}|\p{Emoji}(?:\p{EMod}|\uFE0F\u20E3?|[\u{E0020}-\u{E007E}]+\u{E007F})?`;
+module.exports = () => new RegExp(r`${seq}(?:\u200D(?:${seq}))*`, 'gu');
