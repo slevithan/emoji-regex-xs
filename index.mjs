3f399dd6ba20930f58e27875fdc77e7c2949c5fc
Steven Levithan <steves_list@hotmail.com>
2025-04-22 14:29:15 +0200

    Stricter RI

--- index.mjs
+++ index.mjs
@@ -1,3 +1,3 @@
 const r = String.raw,
-  seq = r`(?:\p{RI}{2}|\p{Emoji}(?:\p{EMod}|[\u{E0020}-\u{E007E}]+\u{E007F}|\uFE0F?\u20E3?))`;
-export default () => new RegExp(r`(?![#*\d](?!\uFE0F?\u20E3))${seq}(?:\u200D${seq})*`, 'gu');
+  e = r`\p{Emoji}(?:\p{EMod}|[\u{E0020}-\u{E007E}]+\u{E007F}|\uFE0F?\u20E3?)`;
+export default () => new RegExp(r`\p{RI}{2}|(?![#*\d](?!\uFE0F?\u20E3))${e}(?:\u200D${e})*`, 'gu');
