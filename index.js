0a2a735df7953c6ab29506b672d0caf101249786
fisker Cheung <lionkay@gmail.com>
2025-04-26 01:38:11 +0800

    Inline regular expression (#7)

--- index.js
+++
@@ -1,3 +0,0 @@
-const r = String.raw,
-  e = r`\p{Emoji}(?:\p{EMod}|[\u{E0020}-\u{E007E}]+\u{E007F}|\uFE0F?\u20E3?)`;
-module.exports = () => new RegExp(r`\p{RI}{2}|(?![#*\d](?!\uFE0F?\u20E3))${e}(?:\u200D${e})*`, 'gu');
