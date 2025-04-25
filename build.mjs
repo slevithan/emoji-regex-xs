354458c8ea5ed7b9f198a275eb0fccb70a013be5
fisker <lionkay@gmail.com>
2025-04-25 12:54:12 +0800

    Code style

--- build.mjs
+++ build.mjs
@@ -2,10 +2,10 @@ import fs from 'node:fs/promises';
 import regex from './regex.mjs';
 
 await fs.writeFile(
-  new URL('./index.mjs',import.meta.url),
+  new URL('./index.mjs', import.meta.url),
   `export default()=>/${regex.source}/${regex.flags}`
 );
 await fs.writeFile(
-  new URL('./index.js',import.meta.url),
+  new URL('./index.js', import.meta.url),
   `module.exports=()=>/${regex.source}/${regex.flags}`
 );
