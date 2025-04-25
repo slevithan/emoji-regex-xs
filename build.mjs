ec9bc4f7fe4e42b447af2af8327accef4714705a
Steven Levithan <steves_list@hotmail.com>
2025-04-25 20:03:57 +0200

    Tidy

--- build.mjs
+++ build.mjs
@@ -3,9 +3,10 @@ import regex from './regex.mjs';
 
 await fs.writeFile(
   new URL('./index.mjs', import.meta.url),
-  `export default()=>/${regex.source}/${regex.flags}`
+  `export default () => /${regex.source}/${regex.flags};`
 );
+
 await fs.writeFile(
   new URL('./index.js', import.meta.url),
-  `module.exports=()=>/${regex.source}/${regex.flags}`
+  `module.exports = () => /${regex.source}/${regex.flags};`
 );
